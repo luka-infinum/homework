7eef65d62dba1a832343dd25faa24dfcc2a39cc2
Ivana Prebeg <ivana.prebeg96@gmail.com>
2025-11-06 15:59:52 +0100

    feat: add shows API and mocks (#8)
    Co-authored-by: Ivana <ivana.prebeg@infinum.hr>

---
+++ movie-app/src/__mocks__/shows.ts
@@ -0,0 +1,65 @@
+export const mockList = [
+  {
+    UUID: "1",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "Interstellar",
+    rating: "5",
+  },
+  {
+    UUID: "2",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "The Shawshank Redemption",
+    rating: "1",
+  },
+  {
+    UUID: "3",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "The Dark Knight",
+    rating: "3",
+  },
+  {
+    UUID: "4",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "Inception",
+    rating: "5",
+  },
+  {
+    UUID: "5",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "Pulp Fiction",
+    rating: "2",
+  },
+  {
+    UUID: "6",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "The Matrix",
+    rating: "5",
+  },
+  {
+    UUID: "7",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "Forrest Gump",
+    rating: "5",
+  },
+  {
+    UUID: "8",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "The Godfather",
+    rating: "1",
+  },
+  {
+    UUID: "9",
+    imageUrl:
+      "https://cdn.pixabay.com/photo/2012/11/28/08/54/milky-way-67504_1280.jpg",
+    title: "Gladiator",
+    rating: "5",
+  },
+];
